70417ce84261b25763ecbb3b5f2024bd47575ec5
ASHMITKATHAT <ashmitkathat0@gmail.com>
2026-01-08 15:45:00 +0530

    refactor: update board controller for columns and members

--- src/controllers/boardController.ts
+++ src/controllers/boardController.ts
@@ -1,12 +1,10 @@
 import { Request, Response, NextFunction } from 'express';
 import { logger } from '../config/logger';
-import { createBoardSchema, updateBoardSchema, getBoardSchema } from '../validators/boardValidator';
-import * as BoardService from "../services/boardService";
+import * as BoardService from '../services/boardService';
 
 export const createBoard = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const validatedData = createBoardSchema.parse(req.body);
-    const board = await BoardService.createBoard(validatedData);
+    const board = await BoardService.default.createBoard({ ...req.body, owner: req.user!.userId });
     logger.info(`Board created with ID: ${board._id}`);
     res.status(201).json(board);
   } catch (error) {
@@ -16,7 +14,7 @@ export const createBoard = async (req: Request, res: Response, next: NextFunctio
 
 export const getBoards = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const boards = await BoardService.getAllBoards();
+    const boards = await BoardService.default.getAllBoards();
     res.status(200).json(boards);
   } catch (error) {
     next(error);
@@ -25,12 +23,8 @@ export const getBoards = async (req: Request, res: Response, next: NextFunction)
 
 export const getBoardById = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const boardId = req.params.id;
-    const validatedParams = getBoardSchema.parse({ id: boardId });
-    const board = await BoardService.getBoardById(validatedParams.id);
-    if (!board) {
-      return res.status(404).json({ message: 'Board not found' });
-    }
+    const board = await BoardService.default.getBoardById(req.params.id);
+    if (!board) return res.status(404).json({ message: 'Board not found' });
     res.status(200).json(board);
   } catch (error) {
     next(error);
@@ -39,13 +33,8 @@ export const getBoardById = async (req: Request, res: Response, next: NextFuncti
 
 export const updateBoard = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const boardId = req.params.id;
-    const validatedParams = getBoardSchema.parse({ id: boardId });
-    const validatedData = updateBoardSchema.parse(req.body);
-    const board = await BoardService.updateBoard(validatedParams.id, validatedData);
-    if (!board) {
-      return res.status(404).json({ message: 'Board not found' });
-    }
+    const board = await BoardService.default.updateBoard(req.params.id, req.body);
+    if (!board) return res.status(404).json({ message: 'Board not found' });
     logger.info(`Board updated with ID: ${board._id}`);
     res.status(200).json(board);
   } catch (error) {
@@ -55,14 +44,59 @@ export const updateBoard = async (req: Request, res: Response, next: NextFunctio
 
 export const deleteBoard = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const boardId = req.params.id;
-    const validatedParams = getBoardSchema.parse({ id: boardId });
-    const result = await BoardService.deleteBoard(validatedParams.id);
-    if (!result) {
-      return res.status(404).json({ message: 'Board not found' });
-    }
-    logger.info(`Board deleted with ID: ${boardId}`);
+    const result = await BoardService.default.deleteBoard(req.params.id);
+    if (!result) return res.status(404).json({ message: 'Board not found' });
+    logger.info(`Board deleted with ID: ${req.params.id}`);
     res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const addColumn = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const column = await BoardService.default.addColumn({ ...req.body, board: req.params.id });
+    res.status(201).json(column);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateColumn = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const column = await BoardService.default.updateColumn(req.params.columnId, req.body);
+    if (!column) return res.status(404).json({ message: 'Column not found' });
+    res.status(200).json(column);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteColumn = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const result = await BoardService.default.deleteColumn(req.params.columnId);
+    if (!result) return res.status(404).json({ message: 'Column not found' });
+    res.status(204).send();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const addMember = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const board = await BoardService.default.addMember(req.params.id, req.body.userId);
+    if (!board) return res.status(404).json({ message: 'Board not found' });
+    res.status(200).json(board);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const removeMember = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const board = await BoardService.default.removeMember(req.params.id, req.params.memberId);
+    if (!board) return res.status(404).json({ message: 'Board not found' });
+    res.status(200).json(board);
   } catch (error) {
     next(error);
   }
